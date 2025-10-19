d0190931717d96cdbcafea7ca7dbe0c77d490697
emblem-66 <143352594+emblem-66@users.noreply.github.com>
2025-10-19 21:39:26 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,17 @@ set -xeuo pipefail
 
 
 # rpm fusion
+dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf install -y rpmfusion-free-release-tainted rpmfusion-nonfree-release-tainted
 
 # all codecs
+dnf install -y libavcodec-freeworld 
+dnf install -y --allowerasing ffmpeg
 
 # jellyfin/kodi
+
+dnf install -y jellyfin
+
+
+
 
